181a6dab04e3a03681580331e1c599b80c8e4a05
camilly <127759034+camillysp@users.noreply.github.com>
2024-09-12 10:01:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa_alterntivas");
+const caixaResultado = document.querySelector(".caixa-reultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternaivas:[
+            "Alternativas 1",
+            "Alternativas 2"
+        ]
+},
+{
+    enunciado: "Pergunta 2",
+    alternaivas:[
+        "Alternativas 1",
+        "Alternativas 2"
+    ]
+},
+{
+    enunciado: "Pergunta 3",
+    alternaivas:[
+        "Alternativas 1",
+        "Alternativas 2"
+    ]
+},
+{
+    enunciado: "Pergunta 4",
+    alternaivas:[
+        "Alternativas 1",
+        "Alternativas 2"
+    ]
+},
+{
+    enunciado: "Pergunta 5",
+    alternaivas:[
+        "Alternativas 1",
+        "Alternativas 2"
+    ]
+}
